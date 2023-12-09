3ed656e163fc00e6085109d039f06494f7bd940b
rehcrr <144657239+rehcrr@users.noreply.github.com>
2023-12-10 02:23:30 +0300

    Update main (1).c

--- labs/12/main (1).c
+++ labs/12/main (1).c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 void binary_8(int n) {
-    char binary[11];
-    for (int i = 0; i < 11; i++) {
-        binary[10 - i] = (n >> i) & 1 ? '1' : '0';
+    char binary[33];
+    for (int i = 0; i < 33; i++) {
+        binary[32 - i] = (n >> i) & 1 ? '1' : '0';
     }
-    binary[10] = '\0';
+    binary[32] = '\0';
     printf("%s\n", binary);
 }
 
 int main(void) {
     int n;
-    int answer;
     printf("Enter a number: ");
     scanf("%d", &n);
-    if ((n < 512) && (n > -512)) {
-        printf("Dec representation of %d is: ", n);
-        binary_10_(n);
-    }
+    printf("Dec representation of %d is: ", n);
+    binary_8(n);
     return 0;
 }
