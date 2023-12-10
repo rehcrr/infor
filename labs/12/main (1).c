ff8c5aaeba16fe19fbd33b1a80d84152b667199f
rehcrr <144657239+rehcrr@users.noreply.github.com>
2023-12-10 14:53:46 +0300

    Update main (1).c

--- labs/12/main (1).c
+++ labs/12/main (1).c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 
-void binary_8(int n) {
-    char binary[33];
-    for (int i = 0; i < 33; i++) {
-        binary[32 - i] = (n >> i) & 1 ? '1' : '0';
+void binary_8(int n, int bit) {
+    char binary[bit + 1];
+    for (int i = 0; i < bit + 1; i++) {
+        binary[bit - 1 - i] = (n >> i) & 1 ? '1' : '0';
     }
-    binary[32] = '\0';
-    printf("%s\n", binary);
+    binary[bit] = '\0';
+    printf("%s   \n", binary);
+}
+int counter_bytes(int n) {
+    int count = 2;
+    int st = 2;
+    
+    while (n >= st){
+        st = st * 2;
+        count++;
+    }
+    return count;
 }
 
 int main(void) {
-    int n;
+    int n, bit;
     printf("Enter a number: ");
     scanf("%d", &n);
     printf("Dec representation of %d is: ", n);
-    binary_8(n);
+    if (n < 0){
+        n = n * -1;
+        bit = counter_bytes(n);
+        n = n * -1;
+    }
+    else{
+        bit = counter_bytes(n);
+    }
+    binary_8(n, bit);
     return 0;
 }
